25712b35968090f2f1639b171ef815086d587678
unknown <sswathismmb@gmail.com>
2025-03-30 23:16:15 +0530

    modified backend for login

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -1,12 +1,13 @@
 // const mongoose = require("mongoose");
 import mongoose from "mongoose";
 export const RestaurantSchema = new mongoose.Schema({
-  owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  name: { type: String, required: true },
-//   address: { type: String, required: true },
-//   phone: { type: String, required: true },
-  menu: [{ type: mongoose.Schema.Types.ObjectId, ref: "FoodItem" }],
-//   createdAt: { type: Date, default: Date.now },
+  // owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+
+  restaurant_name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  // menu: [{ type: mongoose.Schema.Types.ObjectId, ref: "FoodItem" }],
+  //   createdAt: { type: Date, default: Date.now },
 });
 
 
